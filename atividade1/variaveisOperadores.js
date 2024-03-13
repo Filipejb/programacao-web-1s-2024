552cda54407a04d6e82742bb712b1645fe0fcc77
Filipejb <127758798+Filipejb@users.noreply.github.com>
2024-03-13 01:21:17 -0300

    Update variaveisOperadores.js

--- atividade1/variaveisOperadores.js
+++ atividade1/variaveisOperadores.js
@@ -1,1 +1,20 @@
 
+let n1 = 30;
+let n2 = 25;
+
+
+let resulAdi = n1 + n2;
+console.log(`Resultado da Adição entre ${n1} e ${n2}: ${resulAdi}`);
+
+
+let resulSub = n1 - n2;
+console.log(`Resultado da Subtração entre ${n1} e ${n2}: ${resulSub}`);
+
+
+let resulMulti = n1 * n2;
+console.log(`Resultado da Multiplicação entre ${n1} e ${n2}: ${resulMulti}`);
+
+
+let resulDiv = n1 / n2;
+console.log(`Resultado da Divisão entre ${n1} e ${n2}: ${resulDiv}`);
+
