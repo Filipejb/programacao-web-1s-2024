55d416f5ffce3f1ef3303682daeb45d31b898ed7
Filipejb <127758798+Filipejb@users.noreply.github.com>
2024-03-28 18:06:25 -0300

    Update matrizTransposta.js

--- atividade3/matrizTransposta.js
+++ atividade3/matrizTransposta.js
@@ -1,1 +1,34 @@
+function transporMatriz(A) {
+    console.log("Matriz original:");
+    imprimirMatriz(A);
+
+    const transposta = [];
+    for (let i = 0; i < A[0].length; i++) {
+        transposta[i] = [];
+        for (let j = 0; j < A.length; j++) {
+            transposta[i].push(A[j][i]);
+        }
+    }
+
+    console.log("Matriz transposta:");
+    imprimirMatriz(transposta);
+}
+
+function imprimirMatriz(matriz) {
+    for (let i = 0; i < matriz.length; i++) {
+        let linha = '';
+        for (let j = 0; j < matriz[i].length; j++) {
+            linha += matriz[i][j] + ' ';
+        }
+        console.log(linha);
+    }
+}
+
+const matrizOriginal = [
+    [1, 2],
+    [4, 5],
+    [7, 8]
+];
+
+transporMatriz(matrizOriginal);
 
