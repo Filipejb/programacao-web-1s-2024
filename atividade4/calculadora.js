50be9df53f926fb82f4dc3721149216a81fa351a
Filipejb <127758798+Filipejb@users.noreply.github.com>
2024-04-10 18:49:07 -0300

    Update calculadora.js

--- atividade4/calculadora.js
+++ atividade4/calculadora.js
@@ -1,1 +1,18 @@
+const somar = (a,b)=> a+b;
+const subtrair = (a,b) => a-b;
+const multiplicar = (a,b) => a*b;
+const dividir = (a,b) =>  {
+
+   if (b === 0){
+     throw new Error("Nao foi possivel efutar a divisao");
+   }
+   return a/b;
+};
+
+ module.exports = {
+   somar,
+   subtrair,
+   multiplicar,
+   dividir
+ };
 
