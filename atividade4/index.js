7aec752c260c52c3637f19ca1a19689bc9e416c6
Filipejb <127758798+Filipejb@users.noreply.github.com>
2024-04-10 18:48:19 -0300

    Update index.js

--- atividade4/index.js
+++ atividade4/index.js
@@ -1,1 +1,42 @@
+const express = require(`express`);
 
+const {somar, subtrair, multiplicar, dividir} = require('./util/calculadora');
+const app = express();
+
+
+
+app.get('/somar/:a/:b', (req, res) =>{
+  const a = parseFloat(req.params.a);
+  const b = parseFloat(req.params.b);
+  const resultado = somar(a,b);
+  res.send(resultado.toString());
+});
+
+app.get('/subtrair/:a/:b', (req, res) =>{
+  const a = parseFloat(req.params.a);
+  const b = parseFloat(req.params.b);
+  const resultado = subtrair(a,b);
+  res.send(resultado.toString());
+});
+
+app.get('/multiplicar/:a/:b', (req, res) =>{
+  const a = parseFloat(req.params.a);
+  const b = parseFloat(req.params.b);
+  const resultado = multiplicar(a,b);
+  res.send(resultado.toString());
+});
+
+app.get('/dividir/:a/:b', (req, res) =>{
+  const a = parseFloat(req.params.a);
+  const b = parseFloat(req.params.b);
+  try{
+    const resultado = dividir(a,b);
+    res.send(resultado.toString());
+  }catch (erro){
+    res.status(400).send(erro.message);
+  }
+});
+
+app.listen(4000, () =>{
+  console.log('Servidor rodando na porta 4000');
+});
