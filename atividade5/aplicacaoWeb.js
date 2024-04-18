b9cb8ceaeb9cc3a674f7a882160efb6c8d13ec52
Filipejb <127758798+Filipejb@users.noreply.github.com>
2024-04-18 10:00:35 -0300

    Update aplicacaoWeb.js

--- atividade5/aplicacaoWeb.js
+++ atividade5/aplicacaoWeb.js
@@ -1,1 +1,42 @@
+const express = require('express');
+const app = express();
+
+
+let estoque = [];
+
+
+app.get('/adicionar/:id/:nome/qtd', (req, res) => {
+    const { id, nome, qtd } = req.params;
+    estoque.push({ id, nome, quantidade: parseInt(qtd) });
+    res.send('Produto adicionado ao estoque com sucesso.');
+});
+
+
+app.get('/listar', (req, res) => {
+    res.json(estoque);
+});
+
+
+app.get('/remover/:id', (req, res) => {
+    const { id } = req.params;
+    estoque = estoque.filter(produto => produto.id !== id);
+    res.send('Produto removido do estoque com sucesso.');
+});
+
+
+app.get('/editar/:id/qtd', (req, res) => {
+    const { id, qtd } = req.params;
+    const produto = estoque.find(produto => produto.id === id);
+    if (produto) {
+        produto.quantidade = parseInt(qtd);
+        res.send('Quantidade do produto editada com sucesso.');
+    } else {
+        res.status(404).send('Produto não encontrado no estoque.');
+    }
+});
+
+
+app.listen(8080, () => {
+    console.log('Servidor rodando na porta 8080...');
+});
 
