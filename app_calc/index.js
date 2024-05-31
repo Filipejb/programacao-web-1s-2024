a0dde7a9f94cefd972f57e932c0cb6c02ddf47f5
Filipejb <127758798+Filipejb@users.noreply.github.com>
2024-05-31 01:29:27 -0300

    Update index.js

--- app_calc/index.js
+++ app_calc/index.js
@@ -1,1 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+const calculadoraRoutes = require('./src/routes/calculadoraRoutes');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/calculadora', calculadoraRoutes);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src', 'views', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
 
