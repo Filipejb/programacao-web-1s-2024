68cb5b7f07377aba9fbddbd13d2456b22c88fd1c
Filipejb <127758798+Filipejb@users.noreply.github.com>
2024-05-31 01:29:49 -0300

    Update calculadoraRoutes.js

--- app_calc/src/routes/calculadoraRoutes.js
+++ app_calc/src/routes/calculadoraRoutes.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const calculadoraController = require('../controllers/calculadoraController');
+
+router.post('/calcular', calculadoraController.calcular);
+
+module.exports = router;
 
