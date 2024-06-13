e1c9bc9b06a70abf9d17bca71c4a868d37149c4d
Filipejb <127758798+Filipejb@users.noreply.github.com>
2024-06-13 08:29:49 -0300

    Update app.js

--- aplicativoWeb/app.js
+++ aplicativoWeb/app.js
@@ -1,26 +1,27 @@
+// app.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const authRoutes = require('./src/routes/auth');
-const sequelize = require('./src/database/connection');
+const financeRoutes = require('./src/routes/financeRoutes'); // Certifique-se de importar as rotas de finanças corretamente
 
 const app = express();
 
 app.use(bodyParser.json());
 
+// Rotas de autenticação
 app.use('/auth', authRoutes);
 
+// Rotas de finanças
+app.use('/finances', financeRoutes); // Certifique-se de usar as rotas de finanças corretamente
+
+// Middleware para lidar com erros
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Algo deu errado!');
+  res.status(500).send('Something broke!');
 });
 
-sequelize.sync().then(() => {
-  app.listen(8900, () => {
-    console.log('Servidor rodando na porta 9900');
-  });
-}).catch(err => {
-  console.error('Erro ao sincronizar banco de dados:', err);
+module.exports = app; // Exporte o objeto app
+
+app.listen(4200, () => {
+  console.log('Server is running on port 4200');
 });
-
-module.exports = app;
-
