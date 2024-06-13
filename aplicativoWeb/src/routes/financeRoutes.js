f84c47aab23cf8c455afe4818866208424890d81
Filipejb <127758798+Filipejb@users.noreply.github.com>
2024-06-13 08:31:28 -0300

    Update financeRoutes.js

--- aplicativoWeb/src/routes/financeRoutes.js
+++ aplicativoWeb/src/routes/financeRoutes.js
@@ -1,9 +1,12 @@
+// src/routes/financeRoutes.js
+
 const express = require('express');
 const router = express.Router();
 const financeController = require('../controllers/financeController');
 
-router.get('/finances', financeController.getAllFinances);
-router.post('/finances', financeController.createFinance);
-router.delete('/finances/:id', financeController.deleteFinance);
+router.get('/', financeController.getAllFinances);
+router.post('/', financeController.createFinance);
+router.put('/:id', financeController.updateFinance);
+router.delete('/:id', financeController.deleteFinance);
 
 module.exports = router;
