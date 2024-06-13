c8bf03d4c2f069b75ba3bf741989033a7da312fe
Filipejb <127758798+Filipejb@users.noreply.github.com>
2024-06-13 08:31:11 -0300

    Update auth.js

--- aplicativoWeb/src/routes/auth.js
+++ aplicativoWeb/src/routes/auth.js
@@ -1,8 +1,11 @@
+// src/routes/auth.js
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authControllers'); // Caminho relativo correto
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+router.post('/logout', authController.logout);
+router.put('/update', authController.updateUser);
 
 module.exports = router;
