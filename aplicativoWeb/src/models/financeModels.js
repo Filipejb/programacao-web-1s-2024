6b5bc870ebf27c461348a2f4a64fd12cc97174b4
Filipejb <127758798+Filipejb@users.noreply.github.com>
2024-06-13 08:32:01 -0300

    Update financeModels.js

--- aplicativoWeb/src/models/financeModels.js
+++ aplicativoWeb/src/models/financeModels.js
@@ -1,22 +1,19 @@
-const db = require('../database/db');
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database/connection').sequelize; // Adicione .sequelize aqui
 
-const Finance = {
-    getAll: (callback) => {
-        db.all('SELECT * FROM finances', [], (err, rows) => {
-            callback(err, rows);
-        });
-    },
-    create: (description, amount, date, callback) => {
-        db.run('INSERT INTO finances (description, amount, date) VALUES (?, ?, ?)',
-            [description, amount, date], function (err) {
-                callback(err, this.lastID);
-            });
-    },
-    delete: (id, callback) => {
-        db.run('DELETE FROM finances WHERE id = ?', [id], (err) => {
-            callback(err);
-        });
-    }
-};
+const Finance = sequelize.define('Finance', {
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  amount: {
+    type: DataTypes.FLOAT,
+    allowNull: false
+  },
+  date: {
+    type: DataTypes.DATE,
+    allowNull: false
+  }
+});
 
 module.exports = Finance;
