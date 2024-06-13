242788000dd828181cf7cd95a773d02c8768d23c
Filipejb <127758798+Filipejb@users.noreply.github.com>
2024-06-13 08:31:46 -0300

    Update User.js

--- aplicativoWeb/src/models/User.js
+++ aplicativoWeb/src/models/User.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../database/connection');
+const { sequelize } = require('../database/connection');
 
 const User = sequelize.define('User', {
   username: {
